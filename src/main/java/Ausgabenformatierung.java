412c0b28db60844804ea1a59acc467e6f8557a28
TrutzX <trutz@oszimt.de>
2020-08-20 12:51:38 +0200

    finish version

---
+++ src/main/java/Ausgabenformatierung.java
@@ -0,0 +1,37 @@
+/**
+ * Lesen Sie das Informationsblatt zur Ausgabeformatierung und bearbeiten Sie die Aufgaben in dem Übungsblatt "AB-Ausgabeformatierung".
+ * Aufgabe
+ *
+ * Achtung: Für jede Aufgabe soll mindestens ein Commit duchgeführt und das Endergebnis mit Hilfe von "push" in das zentrale Repository
+ * hochgeladen werden.
+ *
+ * Aufgaben Sie Moodle.
+ */
+public class Ausgabenformatierung {
+
+    /**
+     * Main Methode, die alle anderen Methoden aufruft
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        // Aufruf der 3 Methoden
+        // Wenn man nur eine Aufgabe ausgeben will, einfach die anderen auskommentieren
+        Aufgabe1();
+        Aufgabe2();
+        Aufgabe3();
+    }
+
+
+    public static void Aufgabe1(){
+
+    }
+
+    public static void Aufgabe2(){
+
+    }
+
+    public static void Aufgabe3(){
+
+    }
+}
